5fa7be8f237642bd43c5e925c09689fc6cff914b
Qing <qingcheng0715@gmail.com>
2023-10-24 00:19:18 -0400

    AA sequences

--- public/script.js
+++ public/script.js
@@ -1,14 +1,27 @@
 const form = document.getElementById("form");
 const FASTA = "fasta";
-let hey; 
 form.addEventListener("submit", submitForm);
-let uploadText = "Upload a FASTA file to begin extracting features from your sequence"; 
+const uploadText = "Upload a FASTA file to begin extracting features from your sequence"; 
+let proteinFrames = $("input[name='frames']:checked").val();
 
-const all = $("#all"); 
-all.on("change", function() {
+$("#all").on("change", function() {
     let boxes = $("input[type='checkbox']");
     boxes.prop("checked", this.checked);
+    hiddenInput();
 });
+
+$("#prot").on("change", hiddenInput); 
+function hiddenInput() {
+    if ($("#prot").is(':checked')) {
+        $("#hidden-input").css('visibility', 'visible');
+        $('input[name="frames"]').change(function() {
+            proteinFrames = $("input[name='frames']:checked").val();
+        });
+    }
+    else {
+        $("#hidden-input").css('visibility', 'hidden');
+    }
+}
 
 function submitForm(e) {
     e.preventDefault();
@@ -21,6 +34,8 @@ function submitForm(e) {
         const checked = $(this).is(':checked');
         formData.append(value, checked);
     });
+    formData.append('protFrames', proteinFrames);
+
     if (file.val() != "") {
         const fasta =  file.prop('files')[0]; 
         if (isValidFASTA(fasta)) {
@@ -29,11 +44,9 @@ function submitForm(e) {
             sendFile(formData); 
         }
         else {      
-            console.log("NOT FASTA!!!");
             $("#upload-text").html("<div class='red'>Uploaded file does not have .fasta extension! </div>" + uploadText);           
         }
     } else {
-        console.log("no file selected");
         $("#upload-text").html(
             "<span class='red'>No file selected! </span>" + uploadText);      
     }
@@ -42,35 +55,6 @@ function isValidFASTA(fasta) {
     console.log(fasta.name.toLowerCase());
     const extension = fasta.name.split('.');
     return extension[extension.length - 1] === FASTA; 
-}
-function handleData(data) {
-    console.log(data); 
-    let div = "<div class = 'space'>"; 
-    if (data.len) {
-        console.log("data.len is not null"); 
-        console.log(data.len);
-        div += `<div>Length: ${data.len}</div>`;
-    }
-    if (data.gc) {
-        div += `<div>GC Ratio: ${data.gc}</div>`;
-    }
-    if (data.gcat.length > 0) {
-        div += `<div>Nucleotide Frequency: ${data.gcat}</div>`;
-    }
-    if (data.compl) {
-        div += `<div>Complement: ${data.compl}</div>`;
-    }
-    if (data.rev) {
-        div += `<div>Reverse: ${data.rev}</div>`;
-    }
-    if (data.revcompl) {
-        div += `<div>Reverse Complement: ${data.revcompl}</div>`;
-    }
-    if (data.prot) {
-        div += `<div>GC Ratio: ${data.prot}</div>`;
-    }
-    div += "</div>"
-    $("#box4").html(div); 
 }
 
 function sendFile(formData) {
@@ -89,4 +73,52 @@ function sendFile(formData) {
     .catch((err) => {
         console.error("Error occurred:", err); 
     })
+}
+
+function handleData(data) {
+    console.log(data); 
+    let div = `<div class = 'space'> > ${data.name} </p>`; 
+    if (data.len) {
+        console.log("data.len is not null"); 
+        console.log(data.len);
+        div += `<p>Length: ${data.len}</p>`;
+    }
+    if (data.gc) {
+        div += `<p>GC Ratio: ${data.gc}</p>`;
+    }
+    if (data.gcat.length > 0) {
+        div += `<p>Nucleotide Frequency: ${data.gcat}</p>`;
+    }
+    if (data.compl) {
+        div += `<p>Complement: ${data.compl}</p>`;
+    }
+    if (data.rev) {
+        div += `<p>Reverse: ${data.rev}</p>`;
+    }
+    if (data.revcompl) {
+        div += `<p>Reverse Complement: ${data.revcompl}</p>`;
+    }
+    if (data.prot) {
+        let aa = Object.entries(data.prot)
+        .map(([key, value]) => `<div>${key}: ${value}</div>`)
+        .join('');
+
+        div += `<div>Amino Acid Seq: ${aa}</div>`;
+    }
+    div += "</div>"
+    $("#box4").html(div); 
+}
+
+function f(aa) {
+    const labels = ["5'3' Frame 1", "5'3' Frame 2", "5'3' Frame 3", 
+                    "3'5' Frame 1", "3'5' Frame 2", "3'5' Frame 3"]; 
+    if (aa.length === 1) {
+        const dictionary = keys.reduce((acc, key, index) => {
+            acc[key] = values[index];
+            return acc;
+          }, {});
+          
+          console.log(dictionary);
+          
+    }
 }
--- server/server.js
+++ server/server.js
@@ -17,8 +17,6 @@ function uploadFiles(req, res) {
   const body = req.body
   console.log("body:", body);
   let fasta_file = req.file;
-  let first = ""; 
-  // console.log(fasta_file);
   fs.readFile(fasta_file.path, 'utf8', (err, data) => {
     if (err) {
       console.error(err);
@@ -26,7 +24,8 @@ function uploadFiles(req, res) {
     }  
     // Parse the FASTA content using biojs-io-fasta
     const sequences = fasta.parse(data);
-    first = sequences[0].seq.toUpperCase(); 
+    let first = sequences[0].seq.toUpperCase(); 
+    let firstName = sequences[0].name; 
     let len = null, gcat = [], gc = null, compl = null, rev = null,
         revcompl = null, prot = null; 
     if (body.len === "true") {
@@ -39,13 +38,13 @@ function uploadFiles(req, res) {
     }
     if (body.gcratio === "true") {
       console.log("in gcratio is true, what is gcat array?: ", gcat);
-      if (gcat.length == 0) {
+      if (gcat.length === 0) {
         gc = gc(first); 
         console.log("gc: ", gc);
       }
       else {
-        gc = Math.round((gcat[1] + gcat[2]) / length(first)); 
-        console.log("gc: ", gc);
+        gc = (gcat[1] + gcat[2]) / length(first) * 100; 
+        gc = gc.toFixed(2) + '%';
       }
     }
     if (body.compl === "true") {
@@ -61,10 +60,26 @@ function uploadFiles(req, res) {
       console.log("rev complement", revcompl);
     }
     if (body.prot === "true") {
-      console.log("proteins... in progress ");
-      prot = "protein seq in progress"
+      const THREE = 3; 
+      const LABELS = ["5'3' Frame 1", "5'3' Frame 2", "5'3' Frame 3", 
+                    "3'5' Frame 1", "3'5' Frame 2", "3'5' Frame 3"];
+      let labs = LABELS.slice(0, body.protFrames);
+      let values = []; 
+      if (body.protFrames <= THREE) {
+        values = aminoAcid(first, body.protFrames); 
+      }
+      else {
+        let a = aminoAcid(first, THREE); 
+        let b = aminoAcid(reverse(first), THREE);
+        values = a.concat(b);
+      }
+      prot = labs.reduce((acc, key, index) => {
+        acc[key] = values[index];
+        return acc;
+      }, {});
     }
     let returnObj = {
+      name: firstName, 
       len: len, 
       gc: gc, 
       gcat: gcat, 
@@ -73,7 +88,6 @@ function uploadFiles(req, res) {
       revcompl: revcompl, 
       prot: prot
     }
-    // console.log("returnObj: ", JSON.stringify(returnObj)); 
     res.status(200).json(returnObj);
   })  
 } 
@@ -135,6 +149,48 @@ function gc(seq) {
   }
   console.log(count);
   return (count / len); 
+}
+
+function aminoAcid(seq, frames) {
+  console.log(seq);
+  seq = seq.replace(/T/g, 'U');
+  let aa = {
+    "UUU": "F", "UUC": "F", "UUA": "L", "UUG": "L", 
+    "UCU": "S", "UCC": "S", "UCA": "S", "UCG": "S",
+    "UAU": "Y", "UAC": "Y", "UAA": "-", "UAG": "-",
+    "UGU": "C", "UGC": "C", "UGA": "-", "UGG": "W",
+    "CUU": "L", "CUC": "L", "CUA": "L", "CUG": "L",
+    "CCU": "P", "CCC": "P", "CCA": "P", "CCG": "P",
+    "CAU": "H", "CAC": "H", "CAA": "Q", "CAG": "Q",
+    "CGU": "R", "CGC": "R", "CGA": "R", "CGG": "R",
+    "AUU": "I", "AUC": "I", "AUA": "I", "AUG": "M",
+    "ACU": "T", "ACC": "T", "ACA": "T", "ACG": "T",
+    "AAU": "N", "AAC": "N", "AAA": "K", "AAG": "K",
+    "AGU": "S", "AGC": "S", "AGA": "R", "AGG": "R",
+    "GUU": "V", "GUC": "V", "GUA": "V", "GUG": "V",
+    "GCU": "A", "GCC": "A", "GCA": "A", "GCG": "A",
+    "GAU": "D", "GAC": "D", "GAA": "E", "GAG": "E",
+    "GGU": "G", "GGC": "G", "GGA": "G", "GGG": "G"
+  }
+  let proteins = []; 
+  for (let f = 1; f <= frames; f++) {
+    let protein = "";
+    for (let i = f - 1; i < seq.length; i += 3) {
+      if (i + 2 < seq.length) {
+        let codon = seq.substring(i, i + 3)
+        protein += aa[codon];
+        if (i + 2 === seq.length - 1 ) {
+          proteins.push(protein); 
+        } 
+      }
+      else {
+        proteins.push(protein); 
+        i = seq.length; 
+      }
+    }
+  }
+  console.log(proteins);
+  return proteins; 
 }
 
       // Access individual sequences
