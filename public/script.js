275c8e9a29c0f7fc37da2deb8a7a84a9ff7a03a6
Qing <qingcheng0715@gmail.com>
2023-11-21 22:11:10 -0500

    Update script.js

--- public/script.js
+++ public/script.js
@@ -61,9 +61,9 @@ function isValidFASTA(fasta) {
 }
 
 function sendFile(formData) {
-    const vercelURL = "https://dna-wizard-gwc.vercel.app"; 
-    // fetch("http://localhost:5000/upload_file", {
-    fetch(`${vercelURL}/upload_file`, {
+    // const vercelURL = "https://dna-wizard-gwc.vercel.app"; 
+    fetch("http://localhost:5000/upload_file", {
+    // fetch(`${vercelURL}/upload_file`, {
         method: 'POST',
         body: formData,
     }).then((res) => {
