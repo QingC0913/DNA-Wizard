dc328656fca280006dc4151085892a5741228cff
Qing <qingcheng0715@gmail.com>
2023-11-23 23:14:01 -0500

    .cjs

--- server.cjs
+++ server.cjs
@@ -101,7 +101,7 @@ app.get('/', (req, res) => {
 });
 
 // Start the server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`✨🌟Server is running on http://localhost:${PORT}🌟✨`);
 });
